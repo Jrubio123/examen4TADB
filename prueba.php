184c575a558c20228f59983badbaee546ac43202
jrubio18 <juanpablo.rubio990221@gmail.com>
2023-05-02 02:57:54 -0500

    implementando una pequena consulta

--- prueba.php
+++ prueba.php
@@ -20,38 +20,3 @@ foreach ($resultados as $resultado) {
   var_dump($resultado);
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Consulta de subsidios</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-	<h2>Consulta de subsidios</h2>
-	<table class="table">
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>ID Beneficiario</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($resultados as $resultado): ?>
-				<tr>
-					<td><?php echo $resultado->_id; ?></td>
-					<td><?php echo $resultado->id_beneficiario; ?></td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-</div>
-
-</body>
-</html>
